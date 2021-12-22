e14f526e7c3967195b831c90b6230a8170f8e017
MarkFCain <mark.cain@digital.education.gov.uk>
2021-12-22 14:16:14 +0000

    APR-2775 startup setup reordered to make static files have security headers

--- src/SFA.DAS.RoatpFinance/SFA.DAS.RoatpFinance.Web/Startup.cs
+++ src/SFA.DAS.RoatpFinance/SFA.DAS.RoatpFinance.Web/Startup.cs
@@ -192,13 +192,13 @@ namespace SFA.DAS.RoatpFinance.Web
             }
 
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
             app.UseCookiePolicy();
-            app.UseAuthentication();
             app.UseSession();
             app.UseRequestLocalization();
             app.UseStatusCodePagesWithReExecute("/ErrorPage/{0}");
             app.UseSecurityHeaders();
+            app.UseStaticFiles();
+            app.UseAuthentication();
             app.UseHealthChecks("/health");
             app.UseMvc(routes =>
             {
