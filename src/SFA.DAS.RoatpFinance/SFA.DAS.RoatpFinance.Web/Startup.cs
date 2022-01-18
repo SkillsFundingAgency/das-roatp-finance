de4588c401e536838ea5acc7a12eab337b1666c0
MarkFCain <31033361+MarkFCain@users.noreply.github.com>
2022-01-18 14:14:35 +0000

    APR-2775 startup setup reordered to make static files have security h… (#6)
    * APR-2775 startup setup reordered to make static files have security headers
    
    * APR-2775 added special header to http headers for all file types
    
    * APR-2775 header addition using 'use' with conditional check

--- src/SFA.DAS.RoatpFinance/SFA.DAS.RoatpFinance.Web/Startup.cs
+++ src/SFA.DAS.RoatpFinance/SFA.DAS.RoatpFinance.Web/Startup.cs
@@ -26,10 +26,9 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Threading.Tasks;
+using Microsoft.Extensions.Primitives;
 
 namespace SFA.DAS.RoatpFinance.Web
 {
@@ -192,13 +191,21 @@ namespace SFA.DAS.RoatpFinance.Web
             }
 
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
             app.UseCookiePolicy();
-            app.UseAuthentication();
             app.UseSession();
             app.UseRequestLocalization();
             app.UseStatusCodePagesWithReExecute("/ErrorPage/{0}");
             app.UseSecurityHeaders();
+            app.Use(async (context, next) =>
+            {
+                if (!context.Response.Headers.ContainsKey("X-Permitted-Cross-Domain-Policies"))
+                {
+                    context.Response.Headers.Add("X-Permitted-Cross-Domain-Policies", new StringValues("none"));
+                }
+                await next();
+            });
+            app.UseStaticFiles();
+            app.UseAuthentication();
             app.UseHealthChecks("/health");
             app.UseMvc(routes =>
             {
