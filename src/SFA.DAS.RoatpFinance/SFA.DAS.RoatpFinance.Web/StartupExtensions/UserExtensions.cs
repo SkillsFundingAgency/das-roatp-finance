3c6cfd07c10fa4296e90964682a06a26085beedc
Peter.Williams <peter.david.williams.1991@gmail.com>
2025-12-01 15:26:30 +0000

    CSP-2115 - Minor changes to fix sonar warnings

--- src/SFA.DAS.RoatpFinance/SFA.DAS.RoatpFinance.Web/StartupExtensions/UserExtensions.cs
+++ src/SFA.DAS.RoatpFinance/SFA.DAS.RoatpFinance.Web/StartupExtensions/UserExtensions.cs
@@ -47,7 +47,7 @@ public static class UserExtensions
     public static string UserId(this ClaimsPrincipal user)
     {
         var upnClaimName = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/upn";
-        var upn = user.Identities.FirstOrDefault().Claims.FirstOrDefault(x => x.Type == upnClaimName || x.Type == "email");
+        var upn = user.Identities.FirstOrDefault()?.Claims.FirstOrDefault(x => x.Type == upnClaimName || x.Type == "email");
         return upn?.Value ?? $"{_unknownGivenName}-{_unknownSurname}";
     }
 
